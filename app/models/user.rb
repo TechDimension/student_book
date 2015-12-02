0b1278be898d76c55d0ae953552e6b2fa511794c
Matthew <27matt37@gmail.com>
2015-12-02 11:30:04 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+require_dependency 'validators/email_validator.rb'
+class User < ActiveRecord::Base
+    before_save :downcase_email
+
+    validates :name,  presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 200 }
+    validates :email, email: true
+    validates :email, uniqueness: { case_sensitive: false }
+    validates :password, length: { minimum: 7 }
+    has_secure_password
+
+    private 
+
+    def downcase_email
+        self.email = email.downcase
+    end
+end
---
+++ db/migrate/20151125111534_add_name_to_teachers.rb
@@ -0,0 +1,4 @@
+class AddNameToTeachers < ActiveRecord::Migration
+  def change
+  end
+end
