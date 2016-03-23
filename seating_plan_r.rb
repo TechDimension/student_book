ed86c304359c20e410e7f5f0eb22d74945234676
Matthew <27matt37@gmail.com>
2016-03-23 11:19:48 +0000

    Seating plan update

--- seating_plan_r.rb
+++ seating_plan_r.rb
@@ -1,17 +1,28 @@
 class Seating
-	def array_to_coords
-		# 1 are available seats.
-		puts 'Enter available seats width, and seats length'
-		seatLayout =  [[0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],
-						[0,0,0,1,0,0,1,1,1,0,0,1,0,0,0,0,0],
-						[0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],
-						[0,0,0,1,0,0,1,1,1,0,0,1,0,0,0,0,0],
-						[0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0],
-						[0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0],
+
+	def initialize
+		# 0 are available seats.
+		puts 'Enter number of available seats'
+		available_seats = gets.strip
+	end
+
+	def seat_layout
+		seat_array =  [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+	    row = 0
+	    for x in  (0..10)
+	        numToLetter(x)
+	        print(l,seat_array[row],l)
+	        row +=1
+	    end
 	end
 
 	def algorithm
@@ -30,12 +41,7 @@ class Seating
 				#and put minimum of 3 spaces away from another noise 3 student
 			end
 			#if seat layout has no more spaces, then allow to be put in the first available empty space. 
-			
 		end
-
-
-
-
 	end
 	def student_list_data
 		puts 'Enter Class name'
@@ -43,7 +49,8 @@ class Seating
 
 		student_list = []
 		id = 1
-		while loopy = true
+		loopy = true 
+		while loopy == true
 			puts 'Enter Student Name'
 			name = gets.strip
 			puts 'Enter noise level of student'
@@ -58,9 +65,49 @@ class Seating
 			else
 				puts 'Continuing..'
 			end
-			student_list << "#{id, name, noise, whitelist, level}"
-			id ++
+			student_list << "#{id}, #{name}, #{level_learning}, #{whitelist}, #{noise}"
+			id +=1
 		end
 		student_list
 	end
+	private 
+		def numToLetter(x)
+		    if x == 0
+		        l = "J"
+		    elsif x == 1
+		        l = "I"
+		    elsif x == 2
+		        l = "H"
+		    elsif x == 3
+		        l = "G"
+		    elsif x == 4
+		        l = "F"
+		    elsif x == 5
+		        l = "E"
+		    elsif x == 6
+		        l = "D"
+		    elsif x == 7
+		        l = "C"
+		    elsif x == 8
+		        l = "B"
+		    elsif x == 9
+		        l = "A"
+		    end
+		end
 end
+
+seats = Seating.new
+
+
+
+
+
+
+
+
+
+
+
+
+
+
