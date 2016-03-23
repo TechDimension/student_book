d4b69fd3e182d2378182bc165236feb7f69a7091
Matthew <27matt37@gmail.com>
2016-03-23 17:40:13 +0000

    Almost done seating gen

--- seating_plan_r.rb
+++ seating_plan_r.rb
@@ -2,27 +2,34 @@ class Seating
 
 	def initialize
 		# 0 are available seats.
+		puts "Enter the max width of seats, as in max amount of seats in one plane"
+		@seats_x = gets.strip.to_i
+		puts "Enter array Height of seats"
+		@seats_y = gets.strip.to_i
 		puts 'Enter number of available seats'
-		available_seats = gets.strip
+		@available_seats = gets.strip.to_i
 	end
 
 	def seat_layout
-		seat_array =  [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-						[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-	    row = 0
-	    for x in  (0..10)
-	        numToLetter(x)
-	        print(l,seat_array[row],l)
-	        row +=1
-	    end
+		row_array = []
+		seat_array =  []
+		index_row = []
+	    for y in (0..@seats_y-1)
+		    for x in  (0..@seats_x-1)
+		    	index_row << x + 1     
+		        row_array << 0
+		    end
+		    if y == 0
+		    	print("|#{index_row}|")
+		    	puts
+		    end
+		    letter = num_to_letter(y)
+		    seat_array << row_array
+		    print(letter,row_array,letter)
+		    row_array = []
+		    puts
+		end
+		seat_array
 	end
 
 	def algorithm
@@ -43,17 +50,40 @@ class Seating
 			#if seat layout has no more spaces, then allow to be put in the first available empty space. 
 		end
 	end
+
+	def manual_list
+		s_list = student_list_data
+		new_layout = seat_layout
+		print(new_layout)
+		puts
+		puts
+		
+		s_list.each |id, value| do 
+			puts "Enter position for #{value[:name]}. Example 5A"
+			position = gets.strip
+			if postion.length == 2 
+				x_pos = letter_to_num(position[0])
+				y_pos = posiiton[1]
+			end
+		end
+		
+
+	end
 	def student_list_data
 		puts 'Enter Class name'
 		classname = gets.strip
 
-		student_list = []
+		student_list = Hash.new
 		id = 1
 		loopy = true 
 		while loopy == true
 			puts 'Enter Student Name'
 			name = gets.strip
+			puts "Student male or female m/f"
+			gender = gets.strip
 			puts 'Enter noise level of student'
+			
+
 			noise = gets.strip
 			puts 'Enter name of student/s to not sit next to'
 			whitelist = gets.strip
@@ -65,39 +95,70 @@ class Seating
 			else
 				puts 'Continuing..'
 			end
-			student_list << "#{id}, #{name}, #{level_learning}, #{whitelist}, #{noise}"
+			student_list.merge!("#{id}"  => [{:name => "#{name}"}, {:gender => "#{gender}"}, {:level => "#{level_learning}"}, {:whitelist => "#{whitelist}"}, {:noise => "#{noise}"}])
 			id +=1
 		end
 		student_list
 	end
 	private 
-		def numToLetter(x)
+		def num_to_letter(x)
+			x.upcase!
 		    if x == 0
-		        l = "J"
-		    elsif x == 1
-		        l = "I"
-		    elsif x == 2
-		        l = "H"
-		    elsif x == 3
-		        l = "G"
-		    elsif x == 4
-		        l = "F"
-		    elsif x == 5
-		        l = "E"
-		    elsif x == 6
-		        l = "D"
-		    elsif x == 7
-		        l = "C"
-		    elsif x == 8
-		        l = "B"
-		    elsif x == 9
 		        l = "A"
+		    elsif x == 1
+		        l = "B"
+		    elsif x == 2
+		        l = "C"
+		    elsif x == 3
+		        l = "D"
+		    elsif x == 4
+		        l = "E"
+		    elsif x == 5
+		        l = "F"
+		    elsif x == 6
+		        l = "G"
+		    elsif x == 7
+		        l = "H"
+		    elsif x == 8
+		        l = "I"
+		    elsif x == 9
+		        l = "J"
 		    end
+		    l
+		end
+
+		def letter_to_num(x)
+		    if x == "A"
+		        l = 0
+		    elsif x == "B"
+		        l = 1
+		    elsif x == "C"
+		        l = 2
+		    elsif x == "D"
+		        l = 3
+		    elsif x == "E"
+		        l = 4
+		    elsif x == "F"
+		        l = 5
+		    elsif x == "G"
+		        l = 6
+		    elsif x == "H"
+		        l = 7
+		    elsif x == "I"
+		        l = 8
+		    elsif x == "J"
+		        l = 9
+		    end
+		    l
 		end
 end
 
 seats = Seating.new
+#seat_layout_for_user = seats.manual_list
 
+print(seats.manual_list)
+
+puts
 
 
 
