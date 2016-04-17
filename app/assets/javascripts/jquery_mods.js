ca6c5785a67aad6a61921c9e601e7e9ca2f36899
Matthew <27matt37@gmail.com>
2016-04-17 03:04:08 +0100

    jquery

--- app/assets/javascripts/jquery_mods.js
+++ app/assets/javascripts/jquery_mods.js
@@ -3,3 +3,49 @@ $(document).ready(function(){
 });
 
 
+$(document).ready(function(){
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop ').fadeIn();
+        } else {
+            $('.scrollToTop ').fadeOut();
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop ').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    
+});
+
+
+$(document).ready(function(){
+    $("#toggleDiv").toggle(10);
+    $("#buttoon").toggle(10);
+    $("#button").click(function(){
+        $("#toggleDiv").toggle(300);
+        $("#button").toggle(10);
+        $("#buttoon").toggle(10);
+        $('html, body').animate({
+            scrollTop: $("#toggleDiv").offset().top
+        }, 500);
+
+
+    });
+});
+
+$(document).ready(function(){
+    $("#buttoon").click(function(){
+        $("#toggleDiv").toggle(300);
+        $("#buttoon").toggle(10);
+        $("#button").toggle(10);
+        $('html, body').animate({
+            scrollTop: $("#toggleDiv").offset().top
+        }, 500);
+
+
+    });
+});
