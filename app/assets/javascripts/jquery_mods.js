c15efa3ebfdf45e5f2781c102c1d6ac4402a449c
Matthew <27matt37@gmail.com>
2016-04-18 07:57:33 +0100

    bowercolorpicker

--- app/assets/javascripts/jquery_mods.js
+++ app/assets/javascripts/jquery_mods.js
@@ -49,3 +49,16 @@ $(document).ready(function(){
 
     });
 });
+
+$(function() {
+    $('#cp8').colorpicker({
+        colorSelectors: {
+            'default': '#777777',
+            'primary': '#337ab7',
+            'success': '#5cb85c',
+            'info': '#5bc0de',
+            'warning': '#f0ad4e',
+            'danger': '#d9534f'
+        }
+    });
+});
