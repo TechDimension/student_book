9f70a56e48b4048cd58319be85e2b24f388dd97a
Matthew <27matt37@gmail.com>
2016-04-19 20:46:05 +0100

    assets

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module StudentBook
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += %w( .svg .eot .woff .ttf .png .jpg .gif)
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'bower_components')
   end
 end
